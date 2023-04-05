10d329699543a9d72220d24d0c359ba5ea155444
Kamil <kamilkhan52@outlook.com>
2023-04-04 18:41:34 -0600

    All Makefile use gcc-9, and pin_kit added

--- setup_sniper.sh
+++ setup_sniper.sh
@@ -4,6 +4,11 @@
 sudo apt-get update
 sudo apt-get install -y build-essential zlib1g-dev libbz2-dev libboost-dev libsqlite3-dev gfortran m4 python2 git mpich gedit gcc-9 g++-9
 
+# Configure the GCC and G++ version 9 compilers for compiling Sniper and the benchmarks
+ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 50 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+ sudo update-alternatives --config gcc
+
 # Set environment variables
 export SNIPER_ROOT=$(pwd)
 export BENCHMARKS_ROOT=$SNIPER_ROOT/benchmarks
@@ -16,3 +21,5 @@ echo "export BENCHMARKS_ROOT=$BENCHMARKS_ROOT" >> ~/.bashrc
 cd $BENCHMARKS_ROOT
 make
 
+# Run test
+./run-sniper -p splash2-radix -i small -n 4 -c small
