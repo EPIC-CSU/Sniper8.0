569d300a29e534de173f9011d52eaabbbc3526bc
Kamil <kamilkhan52>
2023-04-04 19:06:28 -0600

    removed pin_kit and set to download pin and set symbolic link

--- setup_sniper.sh
+++ setup_sniper.sh
@@ -5,8 +5,13 @@ sudo apt-get update
 sudo apt-get install -y build-essential zlib1g-dev libbz2-dev libboost-dev libsqlite3-dev gfortran m4 python2 git mpich gedit gcc-9 g++-9
 
 # Configure the GCC and G++ version 9 compilers for compiling Sniper and the benchmarks
- sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
- sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 50 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 50 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+
+# Install Intel Pin
+wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.22-98547-g7a303a835-gcc-linux.tar.gz
+tar -xvf pin-3.22-98547-g7a303a835-gcc-linux.tar.gz
+ln -s pin-3.22-98547-g7a303a835-gcc-linux pin_kit
 
 # Set environment variables
 export SNIPER_ROOT=$(pwd)
